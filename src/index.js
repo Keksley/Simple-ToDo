83cfcf4d0d2ad4829b8e777d2601e6eff4f979d3
Женя Б <bbevgenia@yandex.ru>
2020-05-26 20:27:36 +0700

    Скрытие страницы приветствия и показ рабочей по клику

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import GreetingPageComponent from './app/components/greeting-page/greeting-page.
 import WorkingPageComponent from './app/components/working-page/working-page.component';
 
 const greetingPage = new GreetingPageComponent();
-console.log(greetingPage.show());
 const workingPage = new WorkingPageComponent();
-workingPage.hide();
+
+const startButton = greetingPage.el.querySelector('.greeting-page__btn');
+startButton.addEventListener('click', () => {
+    greetingPage.hide();
+    workingPage.show();
+})
