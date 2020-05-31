a69ee42bf6f4eebfc1e69d587902862b9336e842
Женя Б <bbevgenia@yandex.ru>
2020-05-31 16:41:46 +0700

    Добавлен словарь "Дни недели"

--- src/app/components/working-page/working-page.component.js
+++ src/app/components/working-page/working-page.component.js
@@ -7,4 +7,14 @@ export default class WorkingPageComponent  extends Component{
   constructor() {
     super('working-page');
   }
+
+  getTodayDate() {
+
+  }
+}
+
+function dateHandler(date) {
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  const day = date.toDateString().slice(8, 10);
 }
---
+++ src/assets/days.js
@@ -0,0 +1,11 @@
+const daysDictionary = {
+    "Mon": "Понедельник",
+    "Tue": "Вторник",
+    "Wed": "Среда",
+    "Thu": "Четверг",
+    "Fri": "Пятница",
+    "Sat": "Суббота",
+    "Sun": "Воскресение"
+};
+
+export default daysDictionary;
